d00a0298e2ec1131eb40a56838fc60219550a9d7
Luciano <lucianorodriguess101@gmail.com>
2024-09-18 22:55:33 -0300

    refactor(product): Improve validation in product domain model

--- internal/domain/product/product.go
+++ internal/domain/product/product.go
@@ -1,6 +1,16 @@
 package product
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrProductNameRequired  = errors.New("o nome do produto é obrigatório")
+	ErrProductPricePositive = errors.New("o preço do produto deve ser positivo")
+	ErrProductCategoryID    = errors.New("o ID da categoria do produto é obrigatório")
+)
 
 type Product struct {
 	ID          uuid.UUID `json:"id"`
@@ -8,4 +18,17 @@ type Product struct {
 	Price       float64   `json:"price"`
 	Description string    `json:"description,omitempty"`
 	CategoryID  uuid.UUID `json:"category_id"`
+}
+
+func (p *Product) Validate() error {
+	if p.Name == "" {
+		return ErrProductNameRequired
+	}
+	if p.Price <= 0 {
+		return ErrProductPricePositive
+	}
+	if p.CategoryID == uuid.Nil {
+		return ErrProductCategoryID
+	}
+	return nil
 }
