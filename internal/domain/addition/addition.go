8bc1833aeae084af635d34068fe5b5b3af02af96
Luciano <lucianorodriguess101@gmail.com>
2024-09-21 14:11:32 -0300

    feat(addition): Add validation for addition name and price

--- internal/domain/addition/addition.go
+++ internal/domain/addition/addition.go
@@ -1,9 +1,29 @@
 package addition
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrAdditionNameRequired  = errors.New("o nome do acréscimo é obrigatório")
+	ErrAdditionPriceRequired = errors.New("o preço do acréscimo deve ser não negativo")
+)
 
 type Addition struct {
 	ID    uuid.UUID `json:"id"`
 	Name  string    `json:"name"`
 	Price float64   `json:"price"`
+}
+
+func (a *Addition) Validate() error {
+
+	if a.Name == "" {
+		return ErrAdditionNameRequired
+	}
+	if a.Price < 0 {
+		return ErrAdditionPriceRequired
+	}
+	return nil
 }
