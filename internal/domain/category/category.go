9423b53cc790bf5dc4d532042a7b52b87e71caf2
Luciano <lucianorodriguess101@gmail.com>
2024-09-21 13:53:06 -0300

    refactor(category): Add validation for category name

--- internal/domain/category/category.go
+++ internal/domain/category/category.go
@@ -1,9 +1,24 @@
 package category
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrCategoryNameRequired = errors.New("o nome da categoria é obrigatório")
+)
 
 type Category struct {
 	ID          uuid.UUID `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description,omitempty"`
+}
+
+func (p *Category) Validate() error {
+	if p.Name == "" {
+		return ErrCategoryNameRequired
+	}
+	return nil
 }
